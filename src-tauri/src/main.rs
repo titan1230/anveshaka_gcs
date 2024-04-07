3f1e876b0e5a93ff93c5169e98ff5e820af1e865
Akshat Jain <akshat00jain@gmail.com>
2024-04-07 22:44:12 +0530

    added fixes for path

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+  let _ = fix_path_env::fix();
+
   tauri::Builder::default()
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
