1434dd90332952326d93ae48b8066863489a19be
Akshat Jain <akshat00jain@gmail.com>
2024-04-16 11:26:24 +0530

    Update GET function in sensor route to handle errors and return appropriate response

--- src/app/api/sensor/route.ts
+++ src/app/api/sensor/route.ts
@@ -1,12 +1,24 @@
 import { NextResponse } from "next/server"
 
 export async function GET(request: Request) {
-    const res = await fetch('http://localhost:4561', {
-        headers: {
-            'Content-Type': 'application/json',
-        }, cache: "no-store"
-    })
-    const data = await res.text()
 
-    return new Response(data)
+    try {
+        const res = await fetch('http://localhost:4561', {
+            headers: {
+                'Content-Type': 'application/json',
+            }, cache: "no-store"
+        })
+        const data = await res.json()
+        return NextResponse.json({
+            "data": data,
+            "code": res.status
+        })
+    } catch (error) { 
+        return NextResponse.json({
+            "data": {
+                "num": "error"
+            },
+            "code": 500
+        })
+    }
 }
