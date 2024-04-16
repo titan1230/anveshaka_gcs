25293a243ca7324ce1b8f8b934067f4293760a09
Akshat Jain <akshat00jain@gmail.com>
2024-04-16 11:28:11 +0530

    Update CamFeed component to include padding in className

--- src/app/components/CamFeed.tsx
+++ src/app/components/CamFeed.tsx
@@ -5,7 +5,7 @@ import roverImg from "../../../public/mars.jpg";
 export default function CamFeed() {
   return (
     <div className='m-4'>
-      <Image src={roverImg} alt="Rover Cam Feed" className='h-3/5'/>
+      <Image src={roverImg} alt="Rover Cam Feed" className='h-3/5 pt-1'/>
     </div>
   )
 }
