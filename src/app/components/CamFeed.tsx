f045075e3ff65e0e5ad8280c2b0fa06aef71a59c
Akshat Jain <akshat00jain@gmail.com>
2024-04-16 11:23:39 +0530

    Update product name in tauri.conf.json and CamFeed component

--- src/app/components/CamFeed.tsx
+++ src/app/components/CamFeed.tsx
@@ -4,6 +4,8 @@ import roverImg from "../../../public/mars.jpg";
 
 export default function CamFeed() {
   return (
-    <Image src={roverImg} alt="Rover Cam Feed" className='p-4 h-3/5'/>
+    <div className='m-4'>
+      <Image src={roverImg} alt="Rover Cam Feed" className='h-3/5'/>
+    </div>
   )
 }
