bc5ef8b00bd3cbe9fa0dc9c31984734009190b13
Akshat Jain <akshat00jain@gmail.com>
2024-08-20 14:48:33 +0530

    feat: Lazy load image in CamFeed and PathPlanning components, and update Rover Cam Feed URL

--- src/app/components/CamFeed.tsx
+++ src/app/components/CamFeed.tsx
@@ -1,16 +1,24 @@
-import Image from 'next/image'
 import React from 'react'
-import roverImg from "../../../public/mars.jpg";
 
 export default function CamFeed() {
   return (
-    <div className='m-4'>
-      <img src="http://192.168.137.237:8088/" alt="Rover Cam Feed" className='h-3/5 pt-1' />
-      {/* <video width="890" height="504" controls={false} preload="none" autoPlay className='h-[504px] w-[890px]'>
-        <source src="./final.mp4" type="video/mp4" className='h-[504px] w-[890px]' />
+    // <div className='m-4'>
+    //   <img src="http://192.168.254.167:8088/feed" alt="Rover Cam Feed" className='h-3/5 pt-1' />
+    //   {/* <video width="890" height="504" controls={false} preload="none" autoPlay className='h-[504px] w-[890px]'>
+    //     <source src="./final.mp4" type="video/mp4" className='h-[504px] w-[890px]' />
 
-        Your browser does not support the video tag.
-      </video> */}
+    //     Your browser does not support the video tag.
+    //   </video> */}
+    // </div>
+
+    <div className="bg-base-100">
+      <div className='card card-compact bg-base-200 m-5'>
+        <div className='card-body'></div>
+        <p className="text-3xl font-bold pl-3">Cam Feed</p>
+        <div className="dataElements ml-5">
+          <img src="http://192.168.254.167:8088/feed" alt="Rover Cam Feed" className='pr-5 pb-5 w-[880px]' />
+        </div>
+      </div>
     </div>
   )
 }
