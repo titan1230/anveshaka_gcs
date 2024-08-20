91866eea2f9af120ada6885525e9d45186e0ded0
Akshat Jain <akshat00jain@gmail.com>
2024-08-20 14:48:26 +0530

    feat: Update fetch URL in telm route

--- src/app/api/telm/route.ts
+++ src/app/api/telm/route.ts
@@ -3,17 +3,17 @@ import { NextResponse } from "next/server"
 export async function GET(request: Request) {
 
     try {
-        const res = await fetch('http://localhost:4561/telm', {
+        const res = await fetch('http://192.168.:8088/', {
             headers: {
                 'Content-Type': 'application/json',
             }, cache: "no-store"
         })
-        const data = await res.json()
+        const data = await res.text()
         return NextResponse.json({
             "data": data,
             "code": res.status
         })
-    } catch (error) { ``
+    } catch (error) {
         return NextResponse.json({
             "data": {
                 "num": "error"
