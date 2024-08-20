c43a2d0cd87c3bc8f5b18d86c232ed6902bc92b1
Akshat Jain <akshat00jain@gmail.com>
2024-08-20 14:49:05 +0530

    feat: Add StarCoords component

---
+++ src/app/components/StarCoords.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const StarCoords = () => {
+  return (
+    <div className='bg-base-100'>
+      <div className='card card-compact bg-base-200 m-5'>
+        <div className='card-body'>
+          <div className='join'>
+            <p className='join-item join-horizontal'>A</p>
+            <p className='join-item join-horizontal'>A</p>
+            <p className='join-item join-horizontal'>A</p>
+            <p className='join-item join-horizontal'>A</p>
+            <p className='join-item join-horizontal'>A</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default StarCoords;
